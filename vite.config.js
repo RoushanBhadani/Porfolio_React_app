08f5350a5b6a5625b9eb2023ceb2263b22aab660
RoushanWebpristine <roushanwebpristine@gmail.com>
2025-08-03 20:06:05 +0530

    integrate security

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,40 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import obfuscator from 'vite-plugin-javascript-obfuscator';
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     obfuscator(
+//       {
+//         compact: true,
+//         controlFlowFlattening: true,
+//         controlFlowFlatteningThreshold: 0.75,
+//         deadCodeInjection: true,
+//         deadCodeInjectionThreshold: 0.4,
+//         debugProtection: true,
+//         debugProtectionInterval: true,
+//         disableConsoleOutput: true,
+//         rotateStringArray: true,
+//         stringArray: true,
+//         stringArrayEncoding: ['base64'],
+//         stringArrayThreshold: 0.75,
+//         selfDefending: true,
+//       },
+//       ['**/*.js'] 
+//     ),
+//   ],
+//   build: {
+//     minify: false, 
+//     outDir: 'build', 
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import obfuscator from 'vite-plugin-javascript-obfuscator';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    obfuscator(
-      {
-        compact: true,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 0.75,
-        deadCodeInjection: true,
-        deadCodeInjectionThreshold: 0.4,
-        debugProtection: true,
-        debugProtectionInterval: true,
-        disableConsoleOutput: true,
-        rotateStringArray: true,
-        stringArray: true,
-        stringArrayEncoding: ['base64'],
-        stringArrayThreshold: 0.75,
-        selfDefending: true,
-      },
-      ['**/*.js'] 
-    ),
-  ],
-  build: {
-    minify: false, 
-    outDir: 'dist', 
-  },
+  plugins: [react()],
+  server: {
+    open: true,
+  }
 });
